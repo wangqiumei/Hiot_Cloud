99caa0b88d0b3b4e531fa32423a3a33a4f121559
wangqiumei <547298441@qq.com>
2020-05-12 21:44:07 +0800

    3.2添加Json转换测试类

---
+++ app/src/main/java/com/huatec/hiot_cloud/test/networktest/Student.java
@@ -0,0 +1,49 @@
+package com.huatec.hiot_cloud.test.networktest;
+
+import java.io.Serializable;
+
+
+/**
+ * 学生实体类
+ */
+class Student implements Serializable {
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private int age;
+
+    /**
+     * 婚姻状况
+     */
+    private boolean married;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+}
